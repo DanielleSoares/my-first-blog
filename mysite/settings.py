a453ab8346096ff62d0531770b859f249995e0f5
Danielle <asdaniellesoares@hotmail.com>
2018-11-24 12:19:12 -0300

    'Correcao'

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xant1%!z#ce-2bfzzw*yv7lza_nxac5cs@&r+*d03on2vo*owc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
